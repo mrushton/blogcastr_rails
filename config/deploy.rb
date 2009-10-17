274e5630dbfba2ddc70a8f86d1f72650f68b2aa2
Matt Rushton <mrushton@blogcastr.com>
2009-10-16 22:44:03 -0400

    Capistrano Deploy
    Fixed up Capistrano deploy.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,22 +1,36 @@
-#MVR - needed to give us a terminal
+#MVR - tty and ssh settings
 default_run_options[:pty] = true
-#MVR - for access to github
 ssh_options[:forward_agent] = true
-#MVR - log in as root
 ssh_options[:keys] = [File.join(ENV["HOME"], ".ec2", "root.pem")]
-set :user, "root"
-set :application, "rails"
-set :repository, "git@github.com:blogcastr/rails.git"
-set :deploy_to, "/home/blogcastr/rails"
+
+set :application, "blogcastr"
+set :user, "deploy"
+set :use_sudo, false
+
 #MVR - git options
 set :scm, :git
+set :repository, "git@github.com:blogcastr/blogcastr.git"
 set :branch, "master"
 set :deploy_via, :remote_cache
+
 role :web, "blogcastr.com"
 role :app, "blogcastr.com"
 role :db,  "blogcastr.com", :primary => true
+
+#MVR - need to set the permissions on the tmp directory so Passenger can remove the restart file
+after "deploy:finalize_update", :update_tmp_permissions
+task :update_tmp_permissions do
+  run "chmod g+w #{File.join(release_path, 'tmp')}"
+end
+
 namespace :deploy do
+  task :start do
+  end 
+  task :stop do
+  end 
   task :restart, :roles => :app, :except => {:no_release => true} do
-    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+    run "touch #{File.join(current_path, 'tmp', 'restart.txt')}"
+    #MVR - give apache permission to delete file
+    run "chmod g+w #{File.join(current_path, 'tmp', 'restart.txt')}"
   end
 end
