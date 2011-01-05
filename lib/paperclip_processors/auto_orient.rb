cbd4aaff71ed8090a3aa3c59ec46eba45094819c
Matt Rushton <mrushton@blogcastr.com>
2011-01-05 01:20:24 -0500

    Auto Orient
    Fixed image posts to auto orient using a paperclip processor.

---
+++ lib/paperclip_processors/auto_orient.rb
@@ -0,0 +1,16 @@
+module Paperclip
+  class AutoOrient < Paperclip::Processor 
+    def initialize(file, options = {}, *args)
+      @file = file
+    end
+  
+    def make( *args )
+      dst = Tempfile.new([@basename, @format].compact.join("."))
+      dst.binmode
+        
+      Paperclip.run('convert',"#{File.expand_path(@file.path)} -auto-orient #{File.expand_path(dst.path)}")
+      
+      return dst
+    end
+  end
+end
